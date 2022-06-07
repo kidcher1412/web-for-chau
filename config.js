740de43e81848bb961021eaccae90588a5a45cbd
kidcher1412 <kidcher1412@gmail.com>
2022-06-07 18:09:05 +0700

    initial commit

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Tặng Chou 🖤",
+    introTitle: 'Lê Trần Quỳnh Châu',
+    introDesc: `Trái đất vốn lạ thường 🖤
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương ♫`,
+    btnIntro: '^^Thật à^^',
+    title: 'Phải chăng Chou đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng Chou đã say ngay từ lúc thấy nụ cười ấy ♫',
+    btnYes: 'Thích Thông <33',
+    btnNo: 'Chê nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao Chou lại yêu Thông <3',
+    btnReply: 'Gửi cho Chou <3',
+    reply: 'Vì Thông là người cute nhất quả đất <33333333',
+    mess: 'Thông biết mà 🥰. Yêu Chou nhiều nhiều 😘😘',
+    messDesc: 'Tối thứ 6 7h, qua đón Thông đi chơi nha.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/stories/1707291955994896/UzpfSVNDOjMyMzMyMjMxMzE2NDg3Ng==?view_single=true'
+}
